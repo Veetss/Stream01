5e8dac9db89bb2113cef6e514800db562fa1bce5
Vittoria Vassarotti <vit.vassarotti@outlook.it>
2022-12-07 12:46:24 +0100

    Initial commit

---
+++ src/it/develhope/stream/Main.java
@@ -0,0 +1,21 @@
+package it.develhope.stream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String fileName = "src/file.txt";
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+            stream.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
